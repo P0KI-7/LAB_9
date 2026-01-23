17e25574dab4fcdaea0b6fa42845586c268a6c77
P0KI-7 <kmp111207@gmail.com>
2026-01-23 20:40:08 +0300

    add lamda-functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include <string>
+
+// лямба-функции
+auto intKeyFunc = [] (int x) { return x; };
+auto intValueFunc = [] (int x) { return x; };
+
+auto dobleKeyFunc = [] (double x) {
+    if (x < 0) return 0;
+    if (0 <= x && x <= 10) return 1;
+    if (x > 10) return 2;
+};
+auto doubleValueFunc = [] (double x) { return x; };
+
+auto stringKeyFunc = [] (const std::string& x) { return x[0]; };
+auto stringValueFunc = [] (const std::string& x) { return x.length(); };
+
+
+
 
 int main()
 {
